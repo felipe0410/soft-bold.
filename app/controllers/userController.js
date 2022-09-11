0098f3fedb8c4944f8c52040b7e4242c40015eae
1007446687 <andres.montana.r4@gmail.com>
2022-09-10 21:43:01 -0500

    creando vista register y contacto-compra

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -6,8 +6,16 @@ const fs = require('fs');
 const userController ={
    users: function(req, res, next) {
         res.render('./user/Usuario');
-      }
-
-}
+      },
+    contacto : function(req,res,next) {
+      res.render("./user/inf_contacto")
+    },
+    pago: function(req,res,next) {
+      res.render("./user/medios_pago")
+    },
+    registro: function(req,res,next){
+      res.render("./user/registro")
+    }
+  }
 
 module.exports = userController;
--- app/routes/usersRouter.js
+++ app/routes/usersRouter.js
@@ -4,4 +4,9 @@ const userController = require("../controllers/userController")
 
 /* GET users listing. */
 router.get('/', userController.users );
+router.get('/contacto', userController.contacto);
+router.get('/pago', userController.pago);
+router.get('/register', userController.registro);
+
+
 module.exports = router;
